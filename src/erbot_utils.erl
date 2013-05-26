6fcc67c7ae34a3f39cb826047518df7fe15eb73b
Anantha Kumaran <ananthakumaran@gmail.com>
2013-05-26 17:01:23 +0530

    add User Agent header

--- src/erbot_utils.erl
+++ src/erbot_utils.erl
@@ -5,10 +5,9 @@
 gist(Text) ->
     Url = "https://api.github.com/gists",
     Body = {[{public, false},
-	     {files, {[{output.txt, {[{content, list_to_binary(Text)}]}}]}}]},
-    {ok, {_, _Headers, Result}} = httpc:request(post,
-						{Url, "text/json", [],
-						 ejson:encode(Body)}, [], []),
+	     {files, {[{'output.txt', {[{content, list_to_binary(Text)}]}}]}}]},
+    Request = {Url, [{"User-Agent", "Erbot"}], "text/json", ejson:encode(Body)},
+    {ok, {_, _Headers, Result}} = httpc:request(post, Request, [], []),
     {Results} = ejson:decode(Result),
     {_, {[{<<"output.txt">>, {FileAttributes}}]}} = lists:keyfind(<<"files">>, 1, Results),
     {_, GistUrl} = lists:keyfind(<<"raw_url">>, 1, FileAttributes),
